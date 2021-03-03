0da84f9ab800c5aaa4fe4930adacfda87a79e2bb
Leo Dube <15lsd@caslab.queensu.ca>
2021-03-02 20:10:09 -0500

    implemented frontend

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -4,10 +4,10 @@ const roundTo = require("round-to");
 const Game = (props) => {
   const renderGameData = (state, timeLeft) => {
     if (
-      state == "Overtime" ||
-      state == "Period 1" ||
-      state == "Period 2" ||
-      state == "Period 3"
+      state != "Scheduled" ||
+      state != "Final" ||
+      state != "Final (OT)" ||
+      state != "Error"
     ) {
       return (
         <div className="row border border-top-0 justify-content-center mt-0">
@@ -38,21 +38,21 @@ const Game = (props) => {
                 alt=""
               />
             </div>
-            <div className="home-goals col-md-auto px-0 v-center">
+            <div className="home-goals col-lg-auto px-0 v-center mt-2 mt-lg-0">
               <h4 className="goals m-auto">{props.teams.home.goals}</h4>
             </div>
             <div className="home-details col my-auto ml-3">
-              <h3 className="team-name text-right mt-2">
+              <h3 className="team-name text-center text-lg-right mt-2">
                 {props.teams.home.name}
               </h3>
-              <p className="powerplays font-weight-light text-right mb-0">
+              <p className="powerplays font-weight-light text-center text-lg-right mb-0">
                 Powerplay opportunities:{" "}
                 <strong>{props.teams.home.powerplays}</strong>
               </p>
-              <p className="chance-of-pp font-weight-light text-right mb-2">
+              <p className="chance-of-pp font-weight-light text-center text-lg-right mb-2">
                 Chance of getting next powerplay
               </p>
-              <h4 className="powerplay-odds text-right">
+              <h4 className="powerplay-odds text-center text-lg-right">
                 {getPPOdds("home", props.penaltyOnHomeTeamOdds)}%
               </h4>
             </div>
@@ -60,28 +60,28 @@ const Game = (props) => {
         </div>
         <div className="away-block col py-2">
           <div className="row">
-            <div className="away-logo col-4 order-md-3 p-0 m-auto">
+            <div className="away-logo col-4 order-lg-3 p-0 m-auto">
               <img
                 className="img-fluid ml-auto"
                 src={props.teams.away.logo}
                 alt=""
               />
             </div>
-            <div className="away-goals col-md-auto order-md-2 px-0 v-center">
+            <div className="away-goals col-lg-auto order-lg-2 px-0 v-center mt-2 mt-lg-0">
               <h4 className="goals m-auto">{props.teams.away.goals}</h4>
             </div>
-            <div className="away-details col order-md-1 my-auto mr-3">
-              <h3 className="team-name text-left mt-2">
+            <div className="away-details col order-lg-1 my-auto mr-3">
+              <h3 className="team-name text-center text-lg-left mt-2">
                 {props.teams.away.name}
               </h3>
-              <p className="powerplays font-weight-light text-left mb-0">
+              <p className="powerplays font-weight-light text-center text-lg-left mb-0">
                 Powerplay opportunities:{" "}
                 <strong>{props.teams.away.powerplays}</strong>
               </p>
-              <p className="chance-of-pp font-weight-light text-left mb-2">
+              <p className="chance-of-pp font-weight-light text-center text-lg-left mb-2">
                 Chance of getting next powerplay
               </p>
-              <h4 className="powerplay-odds text-left">
+              <h4 className="powerplay-odds text-center text-lg-left">
                 {getPPOdds("away", props.penaltyOnHomeTeamOdds)}%
               </h4>
             </div>
--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const About = () => {
-  return <p>About</p>;
+  return (
+    <div className="row justify-content-center">
+      <div className="col-11 col-md-8 mb-3">
+        <h2 className="mt-3 mb-4">About penaltybias.com</h2>
+        <p>
+          This website is based solely off the work done by David Beaudoin,
+          Oliverr Schulte, and Tim B. Swartz. The equation used to make the
+          prediction comes from{" "}
+          <a
+            href="https://www.sfu.ca/~tswartz/papers/penalty.pdf"
+            target="_blank"
+          >
+            their paper on biased penalty calls in the National Hockey League.
+          </a>
+        </p>
+        <p>
+          Code for this project can be found on{" "}
+          <a href="https://github.com/leodube/penalty-bias" target="_blank">
+            my Github.
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
