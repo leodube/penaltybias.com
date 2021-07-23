784c5ae5b4fd73ed766498feb4443f845e1da4f3
Leo Dube <15lsd@caslab.queensu.ca>
2021-07-23 19:01:43 -0400

    forgot prod api link

--- frontend/src/services/predictPenaltyService.js
+++ frontend/src/services/predictPenaltyService.js
@@ -3,8 +3,7 @@ import axios from "axios";
 export default {
   get: async () => {
     let res = await axios.get(
-      // "https://penalty-bias.herokuapp.com/api/predict-penalties"
-      "http://127.0.0.1:5000/api/predict-penalties"
+      "https://penalty-bias.herokuapp.com/api/predict-penalties"
     );
     return res.data || [];
   },
